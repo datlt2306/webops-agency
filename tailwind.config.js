df393e802039ebca7fe81ecc1a70891a76573dd1
haibvph33557 <haibvph33557@fpt.edu.vn>
2024-05-11 12:45:06 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
                 Nunito: ["Nunito", "sans - serif"],
             }
         },
+        screens: {
+            'sm': '393px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        }
     },
     plugins: [],
 }
