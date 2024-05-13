e36f7f3f427001664ee468e95c694111cac0f546
Luong <luongvqph33534@fpt.edu.vn>
2024-05-14 00:13:03 +0700

    HomePage: xong section: Đối tác, Báo chí, Điều gì

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
+
   theme: {
     extend: {
       boxShadow: {
         custom: "0 4px 8px 0 rgba(0, 0, 0, 0.25)",
+        custom2: "3px 3pxpx 6px 0 rgba(0, 0, 0, 0.2)",
       },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
       },
     },
+    plugins: [require("flowbite/plugin")],
   },
   plugins: [],
 };
