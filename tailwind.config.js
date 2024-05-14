ccadfa000cda3f751e4a996767115d6cdab593d1
haibvph33557 <haibvph33557@fpt.edu.vn>
2024-05-14 10:18:19 +0700

    mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
             'sm': '393px',
             'md': '768px',
             'lg': '1024px',
-            'xl': '1280px',
+            'xl': '1408px',
             '2xl': '1536px',
         }
     },
