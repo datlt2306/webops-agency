1d071c04b0b41c5d28942f29a813bd343280b1a3
Luong <luongvqph33534@fpt.edu.vn>
2024-06-16 08:32:25 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,8 @@ module.exports = {
     },
     screens: {
       sm: "393px",
-      md: "768px",
+      618: "618px",
+      md: "834px",
       lg: "1024px",
       xl: "1408px",
       "2xl": "1536px",
