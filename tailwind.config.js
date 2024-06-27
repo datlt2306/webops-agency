7bb9165f665ee3f961fceb8a6a217c6e2e179100
HoangMinh456 <xinphepgiauten456@gmail.com>
2024-06-27 16:52:06 +0700

    Home-page and Contact-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,29 @@ module.exports = {
       boxShadow: {
         'normal': '0 0 6px 2px rgb(0 0 0 / 0.1)',
         'custom': '0 0 8px 1px rgba(0, 0, 0, 1)',
+        custom: "0 4px 8px 0 rgba(0, 0, 0, 0.25)",
+        custom2: "3px 3pxpx 6px 0 rgba(0, 0, 0, 0.2)",
+        custom3: "0 0 8px 0 rgba(0, 0, 0, 0.2)",
       },
       dropShadow: {
         '3xl': '0 0 5px rgba(0, 0, 0, 0.15)'
 
       },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
       backgroundImage: {
         'icon': "url('./icons/checkmark-circle.svg')",
+        praise: "#264ab9, url('./images/Rectangle 142.png') ",
       }
+
     },
     screens: {
       '393px': '393px',
       '394px': '394px',
       453: "453px",
       '445px': '445px',
+      468: "468px",
       475: "475px",
       '515px': '515px',
       535: "535px",
@@ -36,21 +45,31 @@ module.exports = {
       'md': '768px',
       '780px': '780px',
       '788px': '788px',
+      '834px': "834px",
       '857px': '857px',
       '874px': '874px',
+      890: "890px",
+      926: "926px",
+      999: "999px",
       '932px': '932px',
       'lg': '1024px',
+      1091: "1091px",
+      1100: "1100px",
       '1180px': '1180px',
       1200: "1200px",
       'xl': '1280px',
+      1300: "1300px",
       '1375px': '1375px',
+      '1408px': "1408px",
       '1450px': '1450px',
+      1457: "1457px",
       1466: "1466px",
       '1500px': '1500px',
       '1748px': '1748px',
       '2xl': '1536px',
       '1800px': '1800px',
-      '1818px': '1818px'
+      '1818px': '1818px',
+      '21408px': '21408px'
     },
     fontFamily: {
       Nunito: ['Nunito', 'sans-serif']
